44b9a4d5315d304d5ee0c1e1b14732fa412e8661
ffozilbek <fozilbekfayzullayev05@gmail.com>
2024-07-11 00:24:36 +0500

    add back-end side, and new styles

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-const elTelInput = document.querySelector(".form__input-phone");
+let form = document.querySelector("form");
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        document.querySelector(".form__button").value = "Submiting..";
+        let data = new FormData(form);
+        fetch('https://script.google.com/macros/s/AKfycbzNoHbL7mwg0vc8zVg8E9CWltKZ50TVvwMCCsotqjyJfdy3PY10flCo25awc_32L8Q_/exec', {
+                method: "POST",
+                body: data
+            })
+            .then(res => res.text())
+            .then(data => {
+                document.querySelector("#sub").value = "Submit"
+
+            });
+        alert("E'tiboringiz uchun rahmat! Tez orada siz bilan bog'lanamiz.");
+
+    })
