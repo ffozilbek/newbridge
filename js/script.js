3fd160a2d11794aa6246c8aa49c5239c036d96b2
ffozilbek <fozilbekfayzullayev05@gmail.com>
2024-07-11 11:45:16 +0500

    add modal and its animation

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 let form = document.querySelector("form");
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        document.querySelector(".form__button").value = "Submiting..";
         let data = new FormData(form);
         fetch('https://script.google.com/macros/s/AKfycbzNoHbL7mwg0vc8zVg8E9CWltKZ50TVvwMCCsotqjyJfdy3PY10flCo25awc_32L8Q_/exec', {
                 method: "POST",
                 body: data
             })
             .then(res => res.text())
-            .then(data => {
-                document.querySelector("#sub").value = "Submit"
-
-            });
         const elsInput = document.querySelectorAll(".form__input");
         elsInput.forEach(elInput => {
             elInput.value = "";
         })
-        alert("E'tiboringiz uchun rahmat! Tez orada siz bilan bog'lanamiz.");
+
+        // MODAL
+        const elModal = document.querySelector(".modal");
+        elModal.classList.add("modal__open");
+        setTimeout(() => {
+            elModal.classList.remove("modal__open");
+        }, 5000);
 
     })
